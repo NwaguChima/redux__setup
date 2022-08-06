6c5776725839321173333ebf3a46298cde7a7ded
NwaguChima <nwaguchima48@gmail.com>
2022-08-06 03:44:17 +0100

    added styling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./Content.css";
 
 const Content = () => {
   const [contents] = useSelector((state) => state.contents);
@@ -8,12 +9,14 @@ const Content = () => {
   return (
     <div className="card">
       <h2>Contents from state</h2>
-      {contents?.map((content, index) => (
-        <div key={index}>
-          <h3>{content.title}</h3>
-          <p>{content.body}</p>
-        </div>
-      ))}
+      <div className="content">
+        {contents?.map((content, index) => (
+          <div key={index} className="content__item">
+            <h3>{content.title}</h3>
+            <p>{content.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
